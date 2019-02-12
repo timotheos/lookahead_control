a3870977edae9756ae4ffcac63c36ad9cd80c793
timotheos <mathapua@gmail.com>
2019-02-12 14:34:35 +0900

    Added header files

--- src/lookahead_control.cpp
+++ src/lookahead_control.cpp
@@ -1,1 +1,10 @@
+#include <ros/ros.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <Aria/Aria.h>
+
+
+
+
+
 // rostopic pub -1 /RosAria/cmd_vel geometry_msgs/Twist '[0.1, 0.0, 0.0]' '[0.0, 0.0, 0.0]'
