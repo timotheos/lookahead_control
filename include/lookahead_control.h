5027d13baf2d634a3a2ce8a926b791882db48c60
timotheos <mathapua@gmail.com>
2019-05-25 21:03:32 +0900

    Rehaul of code; Doing things the right way: using eigen, using standard messages; Starting with node handles

--- include/lookahead_control.h
+++ include/lookahead_control.h
@@ -1,91 +1,50 @@
 #ifndef LOOKAHEAD_CONTROL_H_
 #define LOOKAHEAD_CONTROL_H_
-#include <nav_msgs/Odometry.h>
-#include <geometry_msgs/Twist.h>
-#include <geometry_msgs/Pose.h>
-// #include <geometry_msgs/Quaternion.h>
-#include <tf/transform_datatypes.h>
-// #include <tf2_ros/transform_broadcaster.h>
-// #include <tf2/LinearMath/Quaternion.h>
-// #include <sensor_msgs/Imu.h>
+
 #include "ros/ros.h"
+#include <Eigen/Dense>
 
+#include <std_msgs/Float64.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <geometry_msgs/Pose.h>
 
+#include <tf/transform_datatypes.h>
 
-class LookaheadControl {
+class LookAheadControl
+{
   private:
-   // ROS 
-   ros::Publisher cmd_vel_pub;
-   ros::Subscriber odom_sub;
-   ros::Subscriber lookahead_point_sub;
-   //  ros::Rate rate(10); // double check this later
+    // ROS
+    // output (Odometry)
 
-   // State variables
-   double state_variable_x_, state_variable_y_;
-   double state_variable_theta_; 
+    // input (cmd_vel)
 
-   // Lookahead point
-   // translation along the centerline from the center of the robot
-   // note that length is in meters
-   double lookahead_point_x_, lookahead_point_y_;
-   double relative_time_;
-   double time_derivative_x_, time_derivative_y_;
+    // 1. State Space Variables (output)
+    std_msgs::Float64 pose_x_;
+    std_msgs::Float64 pose_y_;
+    std_msgs::Float64 pose_theta_;
 
-   // Reference Point coordinates
-   double reference_point_x_, reference_point_y_;
+    // 2. Decoupling matrix
+    // 2.a determinant
+    std_msgs::Float64 decplg_det;
+    // 2.b matrix
+    // typedef Eigen::Array<std_msgs::Float64, 2, 2> Eigen::Array22d64;
+    Eigen::Array22d decplg_matrix;
+    Eigen::Array22d decplg_inverse;
 
-   // decoupling matrix
-   double decoupling_matrix_a_, decoupling_matrix_b_;
-   double decoupling_matrix_c_, decoupling_matrix_d_;
-   double determinant;
-   double decoupling_inverse_[2][2];
-  //  double decoupling_matrix_[2][2];
+    // 3. Output Equations
 
-   // linear feedback
-   double linear_feedback[2];
-
-   // input 
-   double distance_to_goal[2];
-   double desired_point;
-
-   // nonlinear feedback 
-   double feedback_gains[2];
-
-   //
-   double nonlinear_feedback[2]; 
+    // 4. Linear Feedback
 
 
   public:
-   // Get the odometry message
-   void odomCallback(const nav_msgs::OdometryConstPtr& msg);
+    // odometry and cmd_vel messages are passed
+    LookAheadControl(geometry_msgs::TwistStamped cmd_vel, nav_msgs::Odometry odom);
+    // overloaded constructor
+    LookAheadControl();
+    // destructor
+    ~LookAheadControl();
 
-   // Get desired point
-   void poseCallback(const geometry_msgs::TransformStampedConstPtr& msg);
-   
-   void getReferencePointCoordinates();
-   void getDecouplingInverseMatrix();
-   // LookaheadControl Functions
-   // waypoint minus current position3
-  //  void getDistanceToGoal(); 
-   void getLinearFeedback();
-   void getNonlinearFeedback(); // robot input
-   void publishFeedback();
-
-
-   // Constructor
-   LookaheadControl(ros::NodeHandle& node)
-   {
-     cmd_vel_pub = node.advertise<geometry_msgs::Twist>("cmd_vel", 100);
-     ROS_INFO_STREAM("Adding the subscriber: odometry");
-     odom_sub = node.subscribe("odometry", 1000,
-                             &LookaheadControl::odomCallback, this);
-     lookahead_point_sub = node.subscribe("lookahead_point", 1000,
-                             &LookaheadControl::poseCallback, this);
-     node.param<double>("lookahead_point_x", lookahead_point_x_, 0.8);
-     node.param<double>("lookahead_point_y", lookahead_point_y_, 0.0);
-   }
-
-    // Clean up
-    ~LookaheadControl() {}
 };
+
 #endif
--- src/lookahead_control.cpp
+++ src/lookahead_control.cpp
@@ -1,91 +1,169 @@
-#include "lookahead_control.h"
+#include <lookahead_control.h>
 
-  void LookaheadControl::odomCallback(const nav_msgs::OdometryConstPtr& msg)
-  {
-    // Assign x and y state variables 
-    state_variable_x_ = msg->pose.pose.position.x;
-    state_variable_y_ = msg->pose.pose.position.y;
+// class LookaheadControl {
+//   private:
 
-    // convert quarternion to rotation
-    // tf::quaternionMsgToTF(msg->pose.pose.orientation, pose);
-    state_variable_theta_ = tf::getYaw(msg->pose.pose.orientation);
-  }
+//    // ROS 
+//    ros::Publisher cmd_vel_pub;
+//    ros::Subscriber odom_sub;
+//    ros::Subscriber lookahead_point_sub;
+//    //  ros::Rate rate(10); // double check this later
 
-  void LookaheadControl::poseCallback(const geometry_msgs::TransformStampedConstPtr& msg)
-  {
-    // position
-    lookahead_point_x_ = msg->translation.x;
-    lookahead_point_y_ = msg->position.y;
-    // time
-    relative_time_ = msg->c
+//    // State variables
+//    double state_variable_x_;
+//    double state_variable_y_;
+//    double state_variable_theta_; 
 
-  }
+//    // Lookahead point
+//    // translation along the centerline from the center of the robot
+//    // note that length is in meters
+//    double lookahead_point_x_, lookahead_point_y_;
+//    double relative_time_;
+//    double time_derivative_x_, time_derivative_y_;
+
+//    // Reference Point coordinates
+//    double reference_point_x_, reference_point_y_;
+
+//    // decoupling matrix
+//    double decoupling_matrix_a_, decoupling_matrix_b_;
+//    double decoupling_matrix_c_, decoupling_matrix_d_;
+//    double determinant;
+//    double decoupling_inverse_[2][2];
+//   //  double decoupling_matrix_[2][2];
+
+//    // linear feedback
+//    double linear_feedback[2];
+
+//    // input 
+//    double distance_to_goal[2];
+//    double desired_point;
+
+//    // nonlinear feedback 
+//    double feedback_gains[2];
+
+//    //
+//    double nonlinear_feedback[2]; 
 
 
-  void LookaheadControl::getReferencePointCoordinates()
-  {
-    reference_point_x_ = state_variable_x_
-                         + lookahead_point_x_*cos(state_variable_theta_)
-                         - lookahead_point_y_*sin(state_variable_theta_);
-    reference_point_y_ = state_variable_y_
-                         + lookahead_point_x_*sin(state_variable_theta_)
-                         + lookahead_point_y_*cos(state_variable_theta_);
-  }
+//   public:
+//    // Get the odometry message
+//    void odomCallback(const nav_msgs::OdometryConstPtr& msg);
 
-  void LookaheadControl::getDecouplingInverseMatrix()
-  {
-    decoupling_matrix_a_ = cos(state_variable_theta_);
-    decoupling_matrix_b_ = - state_variable_y_ * cos(state_variable_theta_) 
-                              - state_variable_x_ * sin(state_variable_theta_);
-    decoupling_matrix_c_ = sin(state_variable_theta_);
-    decoupling_matrix_d_ = - state_variable_y_ * sin(state_variable_theta_) 
-                              + state_variable_x_ * cos(state_variable_theta_);
+//    // Get desired point
+//    void poseCallback(const geometry_msgs::TransformStampedConstPtr& msg);
+   
+//    void getReferencePointCoordinates();
+//    void getDecouplingInverseMatrix();
+//    // LookaheadControl Functions
+//    // waypoint minus current position3
+//   //  void getDistanceToGoal(); 
+//    void getLinearFeedback();
+//    void getNonlinearFeedback(); // robot input
+//    void publishFeedback();
+
+//     // Clean up
+//     ~LookaheadControl() {}
+// };
+
+  // void LookaheadControl::odomCallback(const nav_msgs::OdometryConstPtr& msg)
+  // {
+  //   // Assign x and y state variables 
+  //   state_variable_x_ = msg->pose.pose.position.x;
+  //   state_variable_y_ = msg->pose.pose.position.y;
+
+  //   // convert quarternion to rotation
+  //   // tf::quaternionMsgToTF(msg->pose.pose.orientation, pose);
+  //   state_variable_theta_ = tf::getYaw(msg->pose.pose.orientation);
+  // }
+
+  // void LookaheadControl::poseCallback(const geometry_msgs::TransformStampedConstPtr& msg)
+  // {
+  //   // position
+  //   lookahead_point_x_ = msg->translation.x;
+  //   lookahead_point_y_ = msg->position.y;
+  //   // time
+  //   relative_time_ = msg->c
+
+  // }
+
+
+  // void LookaheadControl::getReferencePointCoordinates()
+  // {
+  //   reference_point_x_ = state_variable_x_
+  //                        + lookahead_point_x_*cos(state_variable_theta_)
+  //                        - lookahead_point_y_*sin(state_variable_theta_);
+  //   reference_point_y_ = state_variable_y_
+  //                        + lookahead_point_x_*sin(state_variable_theta_)
+  //                        + lookahead_point_y_*cos(state_variable_theta_);
+  // }
+
+  // void LookaheadControl::getDecouplingInverseMatrix()
+  // {
+  //   decoupling_matrix_a_ = cos(state_variable_theta_);
+  //   decoupling_matrix_b_ = - state_variable_y_ * cos(state_variable_theta_) 
+  //                             - state_variable_x_ * sin(state_variable_theta_);
+  //   decoupling_matrix_c_ = sin(state_variable_theta_);
+  //   decoupling_matrix_d_ = - state_variable_y_ * sin(state_variable_theta_) 
+  //                             + state_variable_x_ * cos(state_variable_theta_);
     
-    determinant = decoupling_matrix_a_ * decoupling_matrix_d_
-                  - decoupling_matrix_b_ * decoupling_matrix_c_;
+  //   determinant = decoupling_matrix_a_ * decoupling_matrix_d_
+  //                 - decoupling_matrix_b_ * decoupling_matrix_c_;
 
-    decoupling_inverse_[0][0] =  decoupling_matrix_d_ / determinant;
-    decoupling_inverse_[0][1] = -decoupling_matrix_b_ / determinant;
-    decoupling_inverse_[1][0] = -decoupling_matrix_c_ / determinant;
-    decoupling_inverse_[1][1] =  decoupling_matrix_a_ / determinant;
-  }
+  //   decoupling_inverse_[0][0] =  decoupling_matrix_d_ / determinant;
+  //   decoupling_inverse_[0][1] = -decoupling_matrix_b_ / determinant;
+  //   decoupling_inverse_[1][0] = -decoupling_matrix_c_ / determinant;
+  //   decoupling_inverse_[1][1] =  decoupling_matrix_a_ / determinant;
+  // }
 
-  void LookaheadControl::getLinearFeedback()
-  {
-    (-reference_point_x_);
-    (-reference_point_x_);
-  }
+  // void LookaheadControl::getLinearFeedback()
+  // {
+  //   (-reference_point_x_);
+  //   (-reference_point_x_);
+  // }
 
-  void LookaheadControl::getNonlinearFeedback()
-  {
+  // void LookaheadControl::getNonlinearFeedback()
+  // {
 
-  }
+  // }
 
-  void LookaheadControl::publishFeedback()
-  { 
-    // obtain current position
-    LookaheadControl::getReferencePointCoordinates();
+  // void LookaheadControl::publishFeedback()
+  // { 
+  //   // obtain current position
+  //   LookaheadControl::getReferencePointCoordinates();
 
-    // get inverse decoupling matrix
-    LookaheadControl::getDecouplingInverseMatrix();
+  //   // get inverse decoupling matrix
+  //   LookaheadControl::getDecouplingInverseMatrix();
 
-    // get linear feedback v
-    LookaheadControl::getLinearFeedback();
+  //   // get linear feedback v
+  //   LookaheadControl::getLinearFeedback();
 
-    // calculate for nonlinear feedback 
-    LookaheadControl::getNonlinearFeedback();
+  //   // calculate for nonlinear feedback 
+  //   LookaheadControl::getNonlinearFeedback();
 
-    ros::spinOnce();
-    // rate.sleep;
-  }
+  //   ros::spinOnce();
+  //   // rate.sleep;
+  // }
+
+// Constructor
+LookAheadControl::LookAheadControl(geometry_msgs::TwistStamped cmd_vel, nav_msgs::Odometry odom)
+{
+  // cmd_vel_pub = node.advertise<geometry_msgs::Twist>("cmd_vel", 100);
+  // ROS_INFO_STREAM("Adding the subscriber: odometry");
+  // odom_sub = node.subscribe("odometry", 1000,
+  //                         &LookaheadControl::odomCallback, this);
+  // lookahead_point_sub = node.subscribe("lookahead_point", 1000,
+  //                         &LookaheadControl::poseCallback, this);
+  // node.param<double>("lookahead_point_x", lookahead_point_x_, 0.8);
+  // node.param<double>("lookahead_point_y", lookahead_point_y_, 0.0);
+}
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "lookahead_control");
-  ros::NodeHandle nh;
-  LookaheadControl lookaheadControl(nh);
-  while(ros::ok()){
-    lookaheadControl.getNonlinearFeedback();
-  }
+  // ros::init(argc, argv, "lookahead_control");
+  // ros::NodeHandle nh;
+  // LookaheadControl lookaheadControl(nh);
+  // while(ros::ok()){
+  //   lookaheadControl.getNonlinearFeedback();
+  // }
   
   
 
